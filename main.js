139f3a7049ae611afa3461e78f17dce546035c5d
InmoJl <2698513236@qq.com>
2022-09-23 23:06:30 +0800

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const { Server } = require('socket.io');
+const { Server, Socket } = require('socket.io');
 const http = require('http');
 
 const app = new Koa();
@@ -14,12 +14,11 @@ function exceptionMessage(message) {
     return { success: false, message, data: null };
 }
 
-function successMessage(data = null, message) {
+function successMessage(data, message) {
     return { success: true, message, data };
 }
 
 io.on('connection', (socket) => {
-
     socket.once('disconnect', () => {
         const room = socket.belongRoom;
         if (room) {
@@ -39,7 +38,7 @@ io.on('connection', (socket) => {
     socket.on('createRoom', (room, callback) => {
 
         if (roomMap.has(room)) {
-            callback(exceptionMessage('房间已存在'));
+            callback?.(exceptionMessage('房间已存在'));
             return;
         }
 
@@ -48,33 +47,34 @@ io.on('connection', (socket) => {
         roomMap.set(room, list);
 
         socket.belongRoom = room;
+        socket.join(room);
 
-        callback(successMessage(room));
+        callback?.(successMessage(room));
     });
 
     socket.on('joinRoom', async (room, callback) => {
 
         // 房间号是否合法
         if (typeof room !== 'string' && typeof room !== 'number') {
-            callback(exceptionMessage('房间 ID 不合法'));
+            callback?.(exceptionMessage('房间 ID 不合法'));
             return;
         }
 
         // 判断是否有这个房间
         if (!roomMap.has(room)) {
-            callback(exceptionMessage('房间不存在'));
+            callback?.(exceptionMessage('房间不存在'));
             return;
         }
 
         // 判断是否有这个房间
         if (socket.belongRoom) {
-            callback(exceptionMessage(socket.belongRoom === room ? '已在这个房间' : '已加入其他房间'));
+            callback?.(exceptionMessage(socket.belongRoom === room ? '已在这个房间' : '已加入其他房间'));
             return;
         }
 
         // 房间是否满人
         if (roomMap.get(room).size >= 2) {
-            callback(exceptionMessage('房间已满人'));
+            callback?.(exceptionMessage('房间已满人'));
             return;
         }
 
@@ -84,18 +84,33 @@ io.on('connection', (socket) => {
         // 把房间名称保存在实例
         socket.belongRoom = room;
         // 通知客户端
-        callback({ success: true, message: `加入房间：${room} 成功` });
-
+        callback?.({ success: true, message: `加入房间：${room} 成功` });
+        // 告知别的客户端，加入成功
+        socket.broadcast.emit('syncAction', { type: 'unlock' });
     });
 
     socket.on('flipGrid', (message, callback) => {
-        // 进入房间了
         if (socket.belongRoom) {
-            socket.in(socket.belongRoom).emit('flipGrid', message);
-            callback(successMessage());
+            socket.broadcast.emit('flipGrid', successMessage(message));
+            callback?.(successMessage());
         }
     });
 
+    socket.on('gameOver', () => {
+        socket.broadcast.emit('gameOver');
+    });
+
+    socket.on('shareMap', (map) => {
+        socket.broadcast.emit('shareMap', map);
+    });
+
+    socket.on('unlock', () => {
+        socket.broadcast.emit('unlock');
+    });
+
+    socket.on('syncAction', (message) => {
+        socket.broadcast.emit('syncAction', message);
+    });
 });
 
-httpServer.listen(2333, '127.0.0.1');
+httpServer.listen(2333, '0.0.0.0');
